fb89241bcd26e2163668d58f2fe7ff97f548eddf
aymenyaseen <aimanggl@gmail.com>
2023-11-27 13:22:25 -0800

    clean up

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,29 +1,16 @@
 import Cost from "@/components/Cost";
 import Estimate from "@/components/Estimate";
-import Experts from "@/components/Experts";
 import GetStarted from "@/components/GetStarted";
-import Header from "@/components/Header";
 import Hero from "@/components/Hero";
 import Partners from "@/components/Partners";
 import Projects from "@/components/Projects";
-import ResourceCard from "@/components/ResourceCard"
-import SearchForm from "@/components/SearchForm"
 import Solutions from "@/components/Solutions";
 import Testimonials from "@/components/Testimonials";
-import { Button } from "@/components/ui/button"
-import { getResources, getResourcesPlaylist } from '@/sanity/actions'
+
 
 export const revalidate = 900;
-const Page = async ({ searchParams }: Props) => {
-  const resources = await getResources({
-    query: searchParams?.query || '',
-    category: searchParams?.category || '',
-    page: '1'
-  })
-
-  const resourcesPlaylist = await getResourcesPlaylist();
-  
-  console.log(resourcesPlaylist)
+const Page = () => {
+ 
   return (
     <>
      <Hero />
