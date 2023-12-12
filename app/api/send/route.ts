24a38e004476e924be34bdcf8ee98f1dc35ec035
aymenyaseen <aimanggl@gmail.com>
2023-12-12 10:48:21 -0800

    contact cleanup

--- app/api/send/route.ts
+++ app/api/send/route.ts
@@ -1,22 +1,22 @@
-import { GithubAccessTokenEmail } from '../../../components/email-template';
-import { NextResponse } from 'next/server';
-import { Resend } from 'resend';
+// import { GithubAccessTokenEmail } from '../../../components/email-template';
+// import { NextResponse } from 'next/server';
+// import { Resend } from 'resend';
 
 
 
-export async function GET() {
-    const resend = new Resend(process.env.RESEND_API_KEY);
+// export async function GET() {
+//     const resend = new Resend(process.env.RESEND_API_KEY);
 
-  try {
-    const { data } = await resend.emails.send({
-        from: 'info@micainteriors.com',
-        to: 'contact@micainteriors.com',
-        subject: 'Hello from Next Js',
-        react: GithubAccessTokenEmail({ username: 'zenorocha' })
-    });
-      return NextResponse.json({ data });
-    }
-  catch (error) {
-    return NextResponse.json({ error });
-  }
-}
+//   try {
+//     const { data } = await resend.emails.send({
+//         from: 'info@micainteriors.com',
+//         to: 'contact@micainteriors.com',
+//         subject: 'Hello from Next Js',
+//         react: GithubAccessTokenEmail({ username: 'zenorocha' })
+//     });
+//       return NextResponse.json({ data });
+//     }
+//   catch (error) {
+//     return NextResponse.json({ error });
+//   }
+// }
